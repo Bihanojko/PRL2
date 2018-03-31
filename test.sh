4053027d203177acace9d486b1020807d5480599
Nikola Valesova <bihanojko@localhost.localdomain>
2018-03-31 09:04:42 +0200

    Final version

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Project: Merge-splitting sort
-# Author: Nikola Valesova, xvales02
-# Date: 8. 4. 2018
+# Author:  Nikola Valesova, xvales02
+# Date:    8. 4. 2018
 
 
 # check number of arguments
@@ -11,13 +11,13 @@ if [ $# -ne 2 ]; then
 fi
 
 # compile source files
-/usr/lib64/openmpi/bin/mpic++ --prefix /usr/local/share/OpenMPI -o mss mss.cpp
+mpic++ --prefix /usr/local/share/OpenMPI -o mss mss.cpp
 
 # create file with random numbers
-dd if=/dev/random bs=1 count=$1 of=numbers # 2>>/dev/null
+dd if=/dev/random bs=1 count=$1 of=numbers 2>/dev/null
 
 # execute
-/usr/lib64/openmpi/bin/mpirun --prefix /usr/local/share/OpenMPI -np $2 mss #numbers $2
+mpirun --prefix /usr/local/share/OpenMPI -np $2 mss
 
 # clean
 rm -f mss numbers
